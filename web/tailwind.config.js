71bb6bb9277e16290549e1c06787a4d2a8e55090
jmininel <ju.mininel@hotmail.com>
2023-05-18 15:39:01 -0300

    created Hero, user profile, copyright

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -79,6 +79,9 @@ module.exports = {
       backgroundSize: {
         stripes: '100% 8px',
       },
+      fontSize: {
+        '5xl': '2.5rem',
+      },
       blur: {
         full: '194px',
       },
